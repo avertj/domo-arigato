80d26885a8db059b31aebc0964fabec7c8fe7233
Jérémie <Jrmie@Port-Jeremie>
2014-04-13 15:12:19 +0200

    modif commune sur Field et FieldStatut, commit pour résoudre les conflicts et partir sur une base commune.

--- domo-arigato/src/field/Field.java
+++ domo-arigato/src/field/Field.java
@@ -1,5 +1,6 @@
 package field;
 
+import robot.Robot;
 import lejos.geom.Point;
 
 public class Field {
@@ -12,15 +13,13 @@ public class Field {
 
 		for (int x = 0; x < 3; x++) {
 			for (int y = 0; y < 3; y++) {
-				this.positions[y * 3 + x] = new Point(30 + x * 60, 50 + y * 50);
+				this.positions[y * 3 + x] = new Point(x*50-50, y*60-60);
 				// System.out.println(this.positions[y*3+x].toString());
 			}
 		}
 	}
 
-	public Point getPuck(EnumPuck p) {
-		return this.positions[p.getNum()];
-	}
+	
 
 	public static Field getInstance() {
 		return INSTANCE;
