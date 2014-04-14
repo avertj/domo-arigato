9ba88914e71e97825ff18137f46b73789c38ad83
Jérémie <Jrmie@Port-Jeremie>
2014-04-14 16:14:21 +0200

    Commit du field

---
+++ domo-arigato/src/field/Field.java
@@ -0,0 +1,43 @@
+package field;
+
+import java.awt.Point;
+
+
+
+public class Field {
+	public static final int PUCK_RADIUS = 15;
+	private Point[] positions;
+	private boolean[] present;
+
+	private static Field INSTANCE = new Field();
+
+	private Field() {
+		this.positions = new Point[9];
+
+		for (int y = -1; y < 2; y++) {
+			for (int x = -1; x < 2; x++) {
+				this.positions[(y+1) * 3 + (x+1)] = new Point(50 * x, 60 * (-y));
+			}
+		}
+		this.present=new boolean[9];
+		for(int i=0;i<9;i++){
+			this.present[i]=true;
+		}
+	}
+
+	public Point getPuck(EnumPuck p) {
+		return this.positions[p.getNum()];
+	}
+
+	public static Field getInstance() {
+		return INSTANCE;
+	}
+	
+	public void setPresent(EnumPuck pos, boolean p){
+		this.present[pos.getNum()]=p;
+	}
+	
+	public boolean isPresent(EnumPuck p){
+		return this.present[p.getNum()];
+	}
+}
